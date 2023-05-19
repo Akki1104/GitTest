953d66e03cc0a730f7686118c82e2553c786e148
Akki1104 <ak947314@gmail.com>
2023-05-19 16:01:54 +0530

    "file added"

--- website/students.js
+++ website/students.js
@@ -2,4 +2,10 @@ let students = ['Arjun','Ankit','Ayush','Vipin'];
 students.forEach(fun)
 function fun(){
     console.log("This is an Item");
+    document.write("This is an Item <br>");
+}
+
+const object = {a:1,b:2,c:3,d:4,e:5};
+for (const x in object){
+    console.log(`${x}:${object[x]}`);
 }
